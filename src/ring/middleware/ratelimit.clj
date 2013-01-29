d7856fa05c7e73ad4e419d57378bcebd95352d24
myfreeweb <floatboth@me.com>
2013-01-29 21:16:49 +0400

    this let thing

--- src/ring/middleware/ratelimit.clj
+++ src/ring/middleware/ratelimit.clj
@@ -24,17 +24,17 @@
 (defn wrap-ratelimit
   ([handler] (wrap-ratelimit handler {}))
   ([handler config]
-   (fn [req]
-     (when (not= @last-hour (get-hour))
-       (reset-ratelimit!)
-       (swap! last-hour (fn [_] (get-hour))))
-     (let [config* (merge default-config config)
-           limit (:limit config*)
-           err-handler (:err-handler config*)
-           ip (:remote-addr req)
-           current ((swap! rate-map update-state limit ip) ip)
-           rl-headers {"X-RateLimit-Limit" (str limit)
-                       "X-RateLimit-Remaining" (str (- limit current))}]
-       (let [h (if (< current limit) handler err-handler)
+   (let [config* (merge default-config config)
+         limit (:limit config*)
+         err-handler (:err-handler config*)]
+     (fn [req]
+       (when (not= @last-hour (get-hour))
+         (reset-ratelimit!)
+         (swap! last-hour (fn [_] (get-hour))))
+       (let [ip (:remote-addr req)
+             current ((swap! rate-map update-state limit ip) ip)
+             rl-headers {"X-RateLimit-Limit" (str limit)
+                         "X-RateLimit-Remaining" (str (- limit current))}
+             h (if (< current limit) handler err-handler)
              rsp (h req)]
          (assoc rsp :headers (merge (:headers rsp) rl-headers)))))))
